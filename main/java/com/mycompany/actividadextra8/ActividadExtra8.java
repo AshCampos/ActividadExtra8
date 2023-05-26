5fd4e26d276da7ee19e8186c1800f1bb6d2ccc3e
Tarea <ashlycampos00@aragon.unam.mx>
2023-05-25 22:00:56 -0600

    Se agrega el proyecto

---
+++ main/java/com/mycompany/actividadextra8/ActividadExtra8.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.actividadextra8;
+
+
+public class ActividadExtra8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
